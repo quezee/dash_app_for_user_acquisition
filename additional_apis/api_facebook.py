1af231fb21d453c61da452403f0985b2b2c1097d
Kirill Kuzmin <k.kuzmin@kefirgames.com>
2019-03-11 18:15:19 +0300

    minor upd

--- additional_apis/api_facebook.py
+++ additional_apis/api_facebook.py
@@ -33,6 +33,7 @@ class FB_data:
                  time_range=None, time_range_step=None, time_ranges=None, time_ranges_step=None):
         self.accounts = accounts
         self.raw_data = {}
+        self.endpoint = 'insights'
         self.time_range_list = []
         self.time_ranges_list = []
         self.params = {
@@ -96,7 +97,7 @@ class FB_data:
                 sleep(2)
         sleep(2)
         return req
-    
+
     def connect_and_paginate(self, url, object_id):
         req = self.connect(url, self.params).json()
         print(f'  First req len: {len(req["data"])}')
@@ -109,8 +110,8 @@ class FB_data:
                 self.raw_data[object_id].extend(req['data'])
 
     def get_raw_data(self, object_id):
-        print(f'Account ID: {object_id}')
-        url = URL_BASE.format(OBJECT_ID=f'act_{object_id}', ENDPOINT='insights')
+        print(object_id)
+        url = URL_BASE.format(OBJECT_ID=object_id, ENDPOINT=self.endpoint)
         self.raw_data[object_id] = []
         if self.time_range_list:
             for time_range_str in self.time_range_list:
@@ -134,10 +135,11 @@ class FB_data:
             self.data.loc[null_filt, 'Platform'] = self.data.loc[null_filt, 'account_name'].str.lower().str.extract('(ios|android)', expand=False)
             
     def get_data(self, fill_platform=True):
-        for object_id in self.accounts:
+        for account_id in self.accounts:
+            object_id = f'act_{account_id}'
             self.get_raw_data(object_id)
             for row in self.raw_data[object_id]:
-                row['account_name'] = self.accounts[object_id]
+                row['account_name'] = self.accounts[account_id]
         data = list(chain.from_iterable(self.raw_data.values()))
         self.data = pd.DataFrame.from_dict(data)
         if fill_platform and 'campaign_name' in self.data:
@@ -149,7 +151,10 @@ class FB_adset_meta(FB_data):
     def __init__(self, adset_ids, limit=6000, fields='start_time,end_time,optimization_goal,attribution_spec,billing_event,bid_amount,daily_budget,targeting,status,bid_strategy'):
         
         self.raw_data = []
+        self.endpoint = ''
         self.adset_ids = adset_ids
+        self.time_range_list = []
+        self.time_ranges_list = []
         self.params = {
             'access_token': MARKER,
             'fields': fields,
@@ -157,7 +162,7 @@ class FB_adset_meta(FB_data):
         }
         
     def get_raw_data(self, object_id):
-        print(f'Adset ID: {object_id}')
+        print(object_id)
         url = URL_BASE.format(OBJECT_ID=object_id, ENDPOINT='')
         req = self.connect(url, self.params)
         self.raw_data.append(req.json())
@@ -169,6 +174,33 @@ class FB_adset_meta(FB_data):
         self.data.set_index('id', inplace=True)
     
     
+class FB_audience_meta(FB_data):
     
+    def __init__(self, accounts=ACCOUNTS, limit=6000, fields='name,lookalike_spec,approximate_count'):    
+        self.accounts = accounts
+        self.raw_data = {}
+        self.endpoint = 'customaudiences'
+        self.time_range_list = []
+        self.time_ranges_list = []        
+        self.params = {
+            'access_token': MARKER,
+            'fields': fields,
+            'limit': limit
+        }
+     
     
-                 
+class FB_reach_estimate(FB_data):
+    
+    def __init__(self, targeting_spec, limit=6000):    
+        self.object_id = f'act_{np.random.choice(list(ACCOUNTS.keys()))}'
+        self.endpoint = 'reachestimate'
+        self.params = {
+            'targeting_spec': targeting_spec,
+            'access_token': MARKER,
+            'limit': limit
+        }
+        
+    def get_data(self):
+        url = URL_BASE.format(OBJECT_ID=self.object_id, ENDPOINT=self.endpoint)
+        req = self.connect(url, self.params)
+        return req.json()['data']['users']
