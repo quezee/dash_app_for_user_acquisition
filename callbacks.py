8977b2d0a3fe70b2c612161a2a51fb8266daf1d9
quezee <k.kuzmin@kefirgames.com>
2019-07-01 18:48:43 +0300

    v0.5

--- callbacks.py
+++ callbacks.py
@@ -2,7 +2,6 @@ import dash
 import dash_core_components as dcc
 from dash.dependencies import Input, Output, State
 import dash_html_components as html
-# import dash_table_experiments as dt
 # import plotly.graph_objs as go
 # from plotly import tools
 
@@ -12,6 +11,7 @@ from ch_handler import CHHandler
 ch = CHHandler(config.DB_HOST, config.DB_PORT)
 
 from app import app
+
 
 @app.callback(Output('media', 'options'),
               [Input('date_range', 'start_date'), Input('date_range', 'end_date'),
@@ -27,5 +27,34 @@ def set_media_options(start_date, end_date,
         query += ' AND AppName = {}'.format(repr(app))
     if plat:
         query += ' AND Platform = {}'.format(repr(plat))
-    response = ch.simple_query(query)
-    return [{'label': row[0], 'value': row[0]} for row in response]
+    data = ch.simple_query(query)
+    return data
+
+
+@app.callback([Output("main_table", "data"), Output("main_table", "columns")],
+              [Input('main_submit', 'n_clicks')],
+              [State('date_range', 'start_date'), State('date_range', 'end_date'),
+               State('app', 'value'), State('plat', 'value'), State('media', 'value'),
+               State('main_groupby', 'value')])
+def update_main_table(n_clicks, start_date, end_date,
+                      app, plat, media, groupby):
+    if not groupby:
+        return [], []
+    start_date = start_date + ' 00:00:00'
+    end_date = end_date + ' 23:59:59'
+    if isinstance(groupby, list):
+        groupby = ', '.join(groupby)
+    query = '''
+    SELECT {}, COUNT(AppsFlyerID) as Installs
+    FROM appsflyer.installs
+    WHERE AttributedTouchTime BETWEEN {} AND {}
+    '''.format(groupby, repr(start_date), repr(end_date))
+    if app:
+        query += ' AND AppName = {}'.format(repr(app))
+    if plat:
+        query += ' AND Platform = {}'.format(repr(plat))
+    if media:
+        query += ' AND MediaSource = {}'.format(repr(media))
+    query += ' GROUP BY {}'.format(groupby)
+    data, cols = ch.simple_query(query, True)
+    return data, cols
--- tabs.py
+++ tabs.py
@@ -19,14 +19,14 @@ TABS = [
         html.Div([
             html.Label('Days cohort', style={'font-weight': 'bold', 'font-size': 17}),
             html.Br(),
-            dcc.RadioItems(id='main/cohort', value='7', style={'font-size': 17},
+            dcc.RadioItems(id='main_cohort', value='7', style={'font-size': 17},
                            options=[{'label': coh, 'value': coh} for coh in config.COHORTS])
         ], style={'marginTop': 20}),
 
         html.Div([
             html.Label('Retarget effect', style={'font-weight': 'bold', 'font-size': 17}),
             html.Br(),
-            dcc.RadioItems(id='main/rtg', value='Exclude', style={'font-size': 17},
+            dcc.RadioItems(id='main_rtg', value='Exclude', style={'font-size': 17},
                            options=[{'label': 'Exclude', 'value': 'Exclude'},
                                     {'label': 'Include', 'value': 'Include'}])
         ], style={'marginTop': 20}),
@@ -34,20 +34,18 @@ TABS = [
         html.Div([
             html.Label('Others threshold', style={'font-weight': 'bold', 'font-size': 17}),
             html.Br(),
-            dcc.Input(id='main/others', value=100, type='number')
+            dcc.Input(id='main_others', value=100, type='number')
         ], style={'marginTop': 20}),
 
         html.Div([
-            html.Label('Breakdown', style={'font-weight': 'bold', 'font-size': 17}),
+            html.Label('Group by', style={'font-weight': 'bold', 'font-size': 17}),
             html.Br(),
-            dcc.Dropdown(id='main/break', value='Campaign', multi=True,
+            dcc.Dropdown(id='main_groupby', multi=True,
                          options=[{'label': by, 'value': by} for by in config.GROUPERS])
         ], style={'width': '35%', 'marginTop': 20}),
 
-        html.Div([dash_table.DataTable(id='main/table')]),
-
-
-
+        html.Button(id='main_submit', n_clicks=0, children='Submit'),
+        dash_table.DataTable(id='main_table')
 
 
     ]),
